8d15bcdb8fc1d2569528040037374aeab02de595
LeoMehraban <62709564+LeoMehraban@users.noreply.github.com>
2023-08-19 10:59:42 -0400

    Update

--- src/main/java/com/leomeh/flowerpower/metatileentities/custom/ManaGeneratorTileEntity.java
+++ src/main/java/com/leomeh/flowerpower/metatileentities/custom/ManaGeneratorTileEntity.java
@@ -1,5 +1,7 @@
 package com.leomeh.flowerpower.metatileentities.custom;
 
+import gregtech.api.capability.impl.EnergyContainerHandler;
+import gregtech.api.gui.ModularUI;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.SimpleGeneratorMetaTileEntity;
@@ -7,6 +9,7 @@ import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import gregtech.client.renderer.texture.Textures;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import vazkii.botania.api.mana.IManaCollector;
 
@@ -20,20 +23,25 @@ public class ManaGeneratorTileEntity extends SimpleGeneratorMetaTileEntity {
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity iGregTechTileEntity) {
         return new ManaGeneratorTileEntity(metaTileEntityId, getTier());
     }
-
     public int manaCostPerTier(int tier){
         return tier * 10;
     }
-    /**
-     * Called when this receiver receives mana.
-     *
-     * @param mana
-     */
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+    @Override
+    protected void reinitializeEnergyContainer() {
+        super.reinitializeEnergyContainer();
+        ((EnergyContainerHandler) energyContainer).setSideOutputCondition((facing) -> facing == frontFacing);
+    }
 
     public void reciveMana(int mana){
         System.out.println(mana);
         if (energyContainer.getEnergyStored() <= energyContainer.getEnergyCapacity()) {
-            energyContainer.addEnergy(mana * ((getTier() + 1) * 8L));
+            energyContainer.addEnergy((long) mana * (getTier() + 1));
         }
     }
 }
